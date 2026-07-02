64a6b3a5cc9ffef044899967b558e38387ee63ed
Josmon Noronha <josmonnoronha2004@gmail.com>
2026-07-02 10:14:06 +0530

    changes for apk build

--- constants/api.js
+++ constants/api.js
@@ -1,2 +1,2 @@
 // Change this to your ngrok URL when tunneling from Kaggle
-export const BACKEND_URL = "http://localhost:8000";
+export const BACKEND_URL = "https://glitzy-poncho-fool.ngrok-free.dev";
