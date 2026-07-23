c1612c13b308fba82f72b5c8be586c9d47659fcd
Josmon Noronha <josmonnoronha2004@gmail.com>
2026-07-23 17:24:08 +0530

    added keyboard avoidance on login screen

--- app/login.jsx
+++ app/login.jsx
@@ -99,7 +99,7 @@ export default function Login() {
     <SafeAreaView style={styles.safe} edges={["top", "bottom"]}>
       <KeyboardAvoidingView
         style={{ flex: 1 }}
-        behavior={Platform.OS === "ios" ? "padding" : undefined}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
       >
         <ScrollView
           contentContainerStyle={styles.content}
